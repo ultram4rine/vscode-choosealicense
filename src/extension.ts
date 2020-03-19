6061893c4ed4206661adc8131398d7f6866e5468
ultram4rine <ultramarine730@gmail.com>
2020-03-20 00:30:17 +0400

    added license creation

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 import { Utils } from "./utils";
 
 export async function activate(
@@ -8,7 +10,34 @@ export async function activate(
     "license.choose",
     async () => {
       let licenses = await Utils.getLicenses();
-      vscode.window.showQuickPick(Array.from(licenses, l => l.name));
+
+      let quickPick = vscode.window.createQuickPick();
+
+      quickPick.items = licenses.map(license => {
+        return {
+          label: license.key,
+          detail: license.name
+        };
+      });
+      quickPick.placeholder = "Choose a license from list.";
+      quickPick.onDidChangeSelection(async selection => {
+        quickPick.dispose();
+
+        let selectedLicense = selection[0].label;
+        let licenseText = await Utils.getLicenseText(selectedLicense);
+
+        // TODO: rootPath is deprecated.
+        let currentFolder = vscode.workspace.rootPath;
+        if (currentFolder === undefined) {
+          vscode.window.showErrorMessage("No open folder");
+        } else {
+          vscode.window.showInformationMessage(currentFolder);
+          let licensePath = path.join(currentFolder, "LICENSE");
+          fs.writeFileSync(licensePath, licenseText.body, "utf8");
+        }
+      });
+      quickPick.onDidHide(() => quickPick.dispose());
+      quickPick.show();
     }
   );
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,27 @@
 import * as WebRequest from "web-request";
 
-export interface License {
+interface License {
   key: string;
   name: string;
+}
+
+interface LicenseText {
+  body: string;
 }
 
 export class Utils {
   public static async getLicenses(): Promise<License[]> {
     const url = "https://api.github.com/licenses";
     let data = await WebRequest.json<License[]>(url, {
+      headers: { "User-Agent": "request" }
+    });
+
+    return data;
+  }
+
+  public static async getLicenseText(key: string): Promise<LicenseText> {
+    const url = `https://api.github.com/licenses/${key}`;
+    let data = await WebRequest.json<LicenseText>(url, {
       headers: { "User-Agent": "request" }
     });
 
