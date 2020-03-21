e521dcf1d9ec5e9ee9c4b8c32e5774604615c82a
ultram4rine <ultramarine730@gmail.com>
2020-03-21 19:41:03 +0400

    register setAuthor command

--- src/extension.ts
+++ src/extension.ts
@@ -6,42 +6,43 @@ import { Utils } from "./utils";
 export async function activate(
   context: vscode.ExtensionContext
 ): Promise<void> {
-  let disposable = vscode.commands.registerCommand(
-    "license.choose",
-    async () => {
-      let licenses = await Utils.getLicenses();
+  vscode.commands.registerCommand("license.choose", async () => {
+    let licenses = await Utils.getLicenses();
 
-      let quickPick = vscode.window.createQuickPick();
+    let quickPick = vscode.window.createQuickPick();
 
-      quickPick.items = licenses.map(license => {
-        return {
-          label: license.key,
-          detail: license.name
-        };
-      });
-      quickPick.placeholder = "Choose a license from list.";
-      quickPick.onDidChangeSelection(async selection => {
-        quickPick.dispose();
+    quickPick.items = licenses.map(license => {
+      return {
+        label: license.key,
+        detail: license.name
+      };
+    });
+    quickPick.placeholder = "Choose a license from list.";
+    quickPick.onDidChangeSelection(async selection => {
+      quickPick.dispose();
 
-        let selectedLicense = selection[0].label;
-        let licenseText = await Utils.getLicenseText(selectedLicense);
+      let selectedLicense = selection[0].label;
+      let licenseText = await Utils.getLicenseText(selectedLicense);
 
-        // TODO: rootPath is deprecated.
-        let currentFolder = vscode.workspace.rootPath;
-        if (currentFolder === undefined) {
-          vscode.window.showErrorMessage("No open folder");
-        } else {
-          vscode.window.showInformationMessage(currentFolder);
-          let licensePath = path.join(currentFolder, "LICENSE");
-          fs.writeFileSync(licensePath, licenseText.body, "utf8");
-        }
-      });
-      quickPick.onDidHide(() => quickPick.dispose());
-      quickPick.show();
-    }
-  );
+      // TODO: rootPath is deprecated.
+      let currentFolder = vscode.workspace.rootPath;
+      if (currentFolder === undefined) {
+        vscode.window.showErrorMessage("No open folder");
+      } else {
+        vscode.window.showInformationMessage(currentFolder);
+        let licensePath = path.join(currentFolder, "LICENSE");
+        fs.writeFileSync(licensePath, licenseText.body, "utf8");
+      }
+    });
+    quickPick.onDidHide(() => quickPick.dispose());
+    quickPick.show();
+  });
 
-  context.subscriptions.push(disposable);
+  vscode.commands.registerCommand("license.setAuthor", async () => {
+    const value = await vscode.window.showInputBox({
+      prompt: "Set author for licenses"
+    });
+  });
 }
 
 export function deactivate() {}
