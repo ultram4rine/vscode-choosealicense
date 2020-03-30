f23dc5a3d2e4360cc8e15bab3dd33fb781d4372a
ultram4rine <ultramarine730@gmail.com>
2020-03-30 17:09:38 +0400

    added choosing workspace folder

--- src/extension.ts
+++ src/extension.ts
@@ -24,7 +24,6 @@ export async function activate(
       let selectedLicense = selection[0].label;
       let licenseText = await Utils.getLicenseText(selectedLicense);
 
-      // TODO: get current folder or choose one from list.
       let folders = vscode.workspace.workspaceFolders;
       if (folders === undefined) {
         vscode.window.showErrorMessage("No folder to create a license");
@@ -59,11 +58,19 @@ export async function activate(
           );
         }
 
-        let licensePath = path.join(
-          folders[0].uri.fsPath,
-          `LICENSE${extension}`
-        );
-        fs.writeFileSync(licensePath, licenseText.body, "utf8");
+        let folder: vscode.WorkspaceFolder | undefined;
+        if (folders.length === 1) {
+          folder = folders[0];
+        } else {
+          folder = await vscode.window.showWorkspaceFolderPick();
+        }
+
+        if (folder === undefined) {
+          vscode.window.showErrorMessage("No folder to create a license");
+        } else {
+          let licensePath = path.join(folder.uri.fsPath, `LICENSE${extension}`);
+          fs.writeFileSync(licensePath, licenseText.body, "utf8");
+        }
       }
     });
     quickPick.onDidHide(() => quickPick.dispose());
