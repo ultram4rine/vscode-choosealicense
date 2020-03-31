dc58400d97a9dfea77daa94602d360abe2779e83
ultram4rine <ultramarine730@gmail.com>
2020-03-31 20:12:32 +0400

    deleted not needed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,34 +1,6 @@
 import { window, workspace, ConfigurationTarget } from "vscode";
-import { json } from "web-request";
-
-interface License {
-  key: string;
-  name: string;
-}
-
-interface LicenseText {
-  body: string;
-}
 
 export class Utils {
-  public static async getLicenses(): Promise<License[]> {
-    const url = "https://api.github.com/licenses";
-    let data = await json<License[]>(url, {
-      headers: { "User-Agent": "request" }
-    });
-
-    return data;
-  }
-
-  public static async getLicenseText(key: string): Promise<LicenseText> {
-    const url = `https://api.github.com/licenses/${key}`;
-    let data = await json<LicenseText>(url, {
-      headers: { "User-Agent": "request" }
-    });
-
-    return data;
-  }
-
   public static replaceYear(year: string, lKey: string, lText: string): string {
     switch (lKey) {
       case "agpl-3.0":
