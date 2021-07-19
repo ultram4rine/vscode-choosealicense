3dbb97a4f70c8c963ec4f255a7e084f13c0d90fc
ultram4rine <ultramarine730@gmail.com>
2021-07-19 23:32:17 +0400

    removed unused import

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,7 +3,6 @@ import * as assert from "assert";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
-import { getLicenses } from "../../api";
 import { replaceAuthor, replaceYear } from "../../utils";
 
 suite("Extension Test Suite", () => {
