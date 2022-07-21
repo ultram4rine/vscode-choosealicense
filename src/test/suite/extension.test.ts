d6504d31fb93e7709fca2877a0396d2396f831c8
ultram4rine <ultramarine730@gmail.com>
2022-07-21 20:58:06 +0000

    improve license creating testing

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,5 +1,6 @@
 import * as assert from "assert";
 import * as fs from "fs";
+import * as os from "os";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -130,22 +131,40 @@ describe("Extension Test Suite", () => {
   });
 
   describe("Commands", () => {
-    it("Create license", async () => {
-      chooseLicense();
-      await vscode.commands.executeCommand(
-        "workbench.action.quickOpenSelectNext"
-      );
-      await vscode.commands.executeCommand(
-        "workbench.action.acceptSelectedQuickOpenItem"
-      );
+    describe("License creation", () => {
+      after("Cleanup", () => {
+        fs.unlinkSync(os.homedir().concat("/license.md"));
+      });
 
-      assert.ok(fs.existsSync("license.md"));
-    });
+      it("Create license", async () => {
+        const pr = chooseLicense();
+        await vscode.commands.executeCommand(
+          "workbench.action.quickOpenSelectNext"
+        );
+        await vscode.commands.executeCommand(
+          "workbench.action.acceptSelectedQuickOpenItem"
+        );
 
-    it("Create default license", async () => {
-      await addDefaultLicense();
+        pr.then(() =>
+          // First license in the list is AGPL-3.0.
+          assert.ok(
+            fs
+              .readFileSync(os.homedir().concat("/license.md"))
+              .includes("GNU AFFERO GENERAL PUBLIC LICENSE")
+          )
+        );
+      });
 
-      assert.ok(fs.existsSync("license.md"));
+      it("Create default license", async () => {
+        await addDefaultLicense();
+
+        // Default is Unlicense in this test.
+        assert.ok(
+          fs
+            .readFileSync(os.homedir().concat("/license.md"))
+            .includes("https://unlicense.org")
+        );
+      });
     });
   });
 });
